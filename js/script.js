19c59564d8fe912c95d8031bf44e86656325ad79
cyberkathleen <67641959+cyberkathleen@users.noreply.github.com>
2022-04-19 09:25:55 +0200

    Added quotes array

--- js/script.js
+++ js/script.js
@@ -10,7 +10,88 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: "If you really want to do something, you'll find a way. If you don't, you'll find an excuse",
+    source: "Jim Rohn"
+  },
+  {
+    quote: "Success is nothing more than a few simple disciplines, practiced every day",
+    source: "Jim Rohn"
+  },
+  {
+    quote: "If you focus on success, you'll have stress. But if you pursue excellence, success will be guaranteed.",
+    source: "Deepak Chopra"
+  },
+  {
+    quote: "The dawn right before the sun rises is the darkest.",
+    source: "BTS",
+    citation: "Tomorrow",
+    year: 2014
+  },
+  {
+    quote: "Even if you're not perfect, you're a limited edition.",
+    source: "RM",
+    citation: "Do You",
+    year: 2015
+  },
+  {
+    quote: "The morning will come again. No darkness, no season is eternal.",
+    source: "BTS",
+    citation: "Spring Day",
+    year: 2017
+  },
+  {
+    quote: "No amount of money ever bought a second of time.",
+    source: "Tony Stark",
+    citation: "Avengers: Endgame",
+    year: 2019
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    source: "Theodore Roosevelt"
+  },
+  {
+    quote: "Those who dare to fail miserably can achieve greatly.",
+    source: "John F. Kennedy"
+  },
+  {
+    quote: "Courage isn't having the strength to go on - it is going on when you don't have strength.",
+    source: "Napoleon"
+  },
+  {
+    quote: "Through discipline comes freedom.",
+    source: "Aristotle"
+  },
+  {
+    quote: "If not now, when?",
+    source: "Eckhart Tolle"
+  },
+  {
+    quote: "The journey of a thousand miles begins with a single step.",
+    source: "Lao Tzu"
+  },
+  {
+    quote: "There are always flowers for those who want to see them.",
+    source: "Henri Matisse"
+  },
+  {
+    quote: "Everything you can imagine is real.",
+    source: "Pablo Picasso"
+  },
+  {
+    quote: "Life is 10% what happens to you and 90% how you react to it.",
+    source: "Charles R. Swindoll"
+  },
+  {
+    quote: "All the world's a stage.",
+    source: "William Shakeshpeare"
+  },
+  {
+    quote: "Life begins where fear ends.",
+    source: "Osho"
+  }
+];
 
 
 /***
